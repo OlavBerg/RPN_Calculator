ae1f55e4c99e04082d799c381093157cc0210c59
OlavBerg <berg_olav@yahoo.se>
2021-12-02 18:59:15 +0100

    Made an RPN calculator

--- main.c
+++ main.c
@@ -2,51 +2,72 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+static bool is_digit(char c) {
+    return c >= '0' && c <= '9';
+}
 
 int main(void) {
     
     printf("Welcome to the RPN Calculator!\n");
     printf("Press 'q' to quit.\n\n");
-    
-    while(true) {
+
+    while (true) {
         printf("Enter expression to calculate: ");
 
-        int num1;
-        int num2;
-        char op;
-        scanf("%d %d %c", &num1, &num2, &op);
+        char num_str[16];
+        size_t num_str_ctr = 0;
+        bool num_flag = false;
 
-        int result;
-        bool valid_op = true;
-        switch(op) {
-            case '+':
-            result = num1 + num2;
-            break;
+        while (true) {
+            char c = getchar();
 
-            case '-':
-            result = num1 - num2;
-            break;
+            if (c == '\n') {
+                break;
+            }
 
-            case '*':
-            result = num1 * num2;
-            break;
+            if (is_digit(c)) {
+                num_flag = true;
+                num_str[num_str_ctr++] = c;
+                continue;
+            }
 
-            case '/':
-            result = num1 / num2;
-            break;
+            if (c == ' ') {
+                if (num_flag) {
+                    num_str[num_str_ctr] = '\0';
+                    num_str_ctr = 0;
+                    stack_push(atoi(num_str));
+                    num_flag = false;
+                }
+                continue;
+            }
 
-            default:
-            valid_op = false;
+            int num2 = stack_pop();
+            int num1 = stack_pop();
+            int result;
+
+            switch(c) {
+                case '+':
+                result = num1 + num2;
+                break;
+
+                case '-':
+                result = num1 - num2;
+                break;
+
+                case '*':
+                result = num1 * num2;
+                break;
+
+                case '/':
+                result = num1 / num2;
+                break;
+            }
+
+            stack_push(result);
         }
 
-        if (valid_op) {
-            printf("Result: %d\n", result);
-        }
-        else {
-            printf("ERROR: Invalid operator!\n");
-        }
+        printf("Result: %d\n", stack_pop());
     }
-
-    return 0;
 }
